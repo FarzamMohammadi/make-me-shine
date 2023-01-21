26f331fe7ff51444c116fe229e2ae6da7aa930ea
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-01-21 10:39:03 -0500

    Create typescript svelte app
    Uses tailwind

---
+++ make-me-shine/src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="text-center text-teal-500">Tailwind is working</p>
