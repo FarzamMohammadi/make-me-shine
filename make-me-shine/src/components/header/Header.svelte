91f4b106bfe40a80b2f1cc238b841d8c473f971f
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-01-22 16:09:00 -0500

    Add header & footer

---
+++ make-me-shine/src/components/header/Header.svelte
@@ -0,0 +1,21 @@
+<script>
+   import { Lightbox } from 'svelte-lightbox'
+    import ProfilePic from "$lib/profile-picture.jpeg"
+</script>
+
+<div class="w-full h-full flex flex-col justify-between">
+
+    <header class="h-16 w-full flex items-center relative justify-end px-5 space-x-10 bg-gray-800">
+      <div class="flex flex-shrink-0 items-center space-x-4 text-white">
+        
+        <div class="flex flex-col items-end ">
+          <div class="text-md font-medium ">FARZAM MOHAMMADI</div>
+          <div class="text-sm font-regular">Full-Stack Developer</div>
+        </div>
+        <!-- Profile photo -->
+        <Lightbox description="Tis I">
+          <img class="h-12 w-12 rounded-full cursor-pointer bg-gray-200 border-2 border-blue-400" src={ProfilePic} alt="Profile"/>
+        </Lightbox>
+      </div>
+    </header>
+</div>
--- make-me-shine/src/routes/+layout.svelte
+++ make-me-shine/src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import '../app.postcss';
+	import Header from '../components/header/Header.svelte';
+	import Footer from '../components/footer/Footer.svelte';
 </script>
 
+<Header />
 <slot />
+<Footer />
